1f023c2257b1abaf76a218644506e4d81311317a
Shaam <shaam@shx.shaamx.gov.il>
2019-12-19 15:12:48 +0200

    testing something

--- ng-workspace/projects/forms-tutorial/src/app/message/message.component.spec.ts
+++ ng-workspace/projects/forms-tutorial/src/app/message/message.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { MessageComponent } from './message.component';
+import {RouterTestingModule} from '@angular/router/testing';
 
 describe('MessageComponent', () => {
   let component: MessageComponent;
